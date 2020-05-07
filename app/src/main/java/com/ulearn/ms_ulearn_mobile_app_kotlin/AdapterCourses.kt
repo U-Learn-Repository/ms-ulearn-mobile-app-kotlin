9193ae5832896de8ac92287ca8e128f225223f6c
Johan Medina <josmedinaca@unal.edu.co>
2020-05-06 21:20:09 -0500

    merge courses

---
+++ app/src/main/java/com/ulearn/ms_ulearn_mobile_app_kotlin/AdapterCourses.kt
@@ -0,0 +1,35 @@
+package com.ulearn.ms_ulearn_mobile_app_kotlin
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class AdapterCourses (var ctx:Context, var src: Int, var items: ArrayList<ModelCoursesRow>) : ArrayAdapter<ModelCoursesRow>(ctx, src, items) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val layoutInflater:LayoutInflater = LayoutInflater.from(ctx);
+        val view:View = layoutInflater.inflate(src, null);
+        val imageView:ImageView = view.findViewById(R.id.quiz_image);
+        val titleView:TextView  = view.findViewById(R.id.row_title);
+
+        val myItem: ModelCoursesRow = items[position];
+
+        imageView.setImageDrawable(ctx.resources.getDrawable(myItem.img));
+        titleView.text = myItem.title;
+
+        return view
+    }
+
+    fun update(newItems:  List<ModelCoursesRow>) {
+
+        for(itm in newItems) {
+            this.items.add(itm);
+        }
+
+        notifyDataSetChanged()
+    }
+}
