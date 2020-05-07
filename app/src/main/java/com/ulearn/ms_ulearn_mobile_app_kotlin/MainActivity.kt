513ec079e623f55946aa875dd2a1ee0cfab00f68
Johan Medina <josmedinaca@unal.edu.co>
2020-05-06 20:38:12 -0500

    Revert "Merge branch 'feature/courses' into develop"
    This reverts commit fba9a3e4e3bab367eca4b2dfcec6af7895bec821, reversing
    changes made to c9a7d90f7da935a1ca63c8c041511c02a4a358de.

--- app/src/main/java/com/ulearn/ms_ulearn_mobile_app_kotlin/MainActivity.kt
+++ app/src/main/java/com/ulearn/ms_ulearn_mobile_app_kotlin/MainActivity.kt
@@ -3,23 +3,28 @@ package com.ulearn.ms_ulearn_mobile_app_kotlin
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.internal.Internal
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_go_course.setOnClickListener {
-
-
+        btn_go_quiz.setOnClickListener {
             goQuizzes();
         }
     }
 
     private fun goQuizzes() {
         // val intent = Intent(this, ActivityQuizzes::class.java);
-        val intent = Intent(this, ActivityListCourses::class.java)
+        val intent = Intent(this, ActivityListQuizzes::class.java)
         startActivity(intent);
     }
 }
--- app/src/main/java/com/ulearn/ms_ulearn_mobile_app_kotlin/ModelCoursesRow.kt
+++
@@ -1,4 +0,0 @@
-package com.ulearn.ms_ulearn_mobile_app_kotlin
-
-class ModelCoursesRow (val title:String, val img: Int) {
-}
