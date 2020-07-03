c215d9538ddf0b0817f5ae27521c2f3adc34ec67
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-07-02 22:49:49 -0500

    IP was updated

--- app/src/main/java/com/ulearn/ms_ulearn_mobile_app_kotlin/MainActivity.kt
+++ app/src/main/java/com/ulearn/ms_ulearn_mobile_app_kotlin/MainActivity.kt
@@ -3,7 +3,9 @@ package com.ulearn.ms_ulearn_mobile_app_kotlin
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.orhanobut.logger.AndroidLogAdapter
 import java.io.File
+import com.orhanobut.logger.Logger
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,12 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Logger.addLogAdapter(AndroidLogAdapter())
+
         val filePath = Configuration.file_auth;
 
         val file = File(getExternalFilesDir(filePath), filePath)
 
         if(file.isFile) {
             val tk = file.readText()
+
+            Logger.d(tk.toString());
 
             if(tk!= null && tk.toString().length >= 36) {
                 val intent = Intent(this, MainPanel::class.java)
